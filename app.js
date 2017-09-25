d1caa006627bd6435b97f5ce31a56cb3f630d991
Thomas Santos <thomas@socialeffort.co>
2017-09-24 20:58:25 -0400

    add template for movie layout

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require('express');
 
 //  new express application
 var app = express();
+
+// use express method 'set' to establish the view engine
+app.set("view engine", 'ejs');
 // console.log("MTFBWY");
 
 //////////////
@@ -14,7 +17,12 @@ var app = express();
 // 2nd arg: request and a response
 // for get -- based on req, send some response
 app.get('/', function(req, res) {
-  res.send("There is an awakening...");
+//   res.send("There is an awakening...");
+  res.render('home', {
+      // passing an object literal in here that we can use in our ejs
+    title: "Star Wars Hub",
+    movies: ["One", "Two", "Three"]
+  });
 });
 
 app.get('/darth', function(req, res) {
@@ -29,9 +37,18 @@ app.get('/star_wars_episode/:episode_number?', function(req, res) {
   res.send("This is episode " + episode_number + ".");
 });
 
+// Missing Page
+// This runs if any other route other than what is defined is typed in
+app.get('*', function(req, res) {
+  res.send("This is not the page that you are looking for.");
+});
+
+// Console Log
+// Check if app runsclear
+
 app.listen(1234, function() {
   console.log("This droid is running on localhost:1234");
-})
+});
 
 
 
